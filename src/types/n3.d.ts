81eef6846a47537de921e9af79b8a7985b1e6bfd
jahed.dev <3330963+jahed@users.noreply.github.com>
2025-07-26 15:10:28 +0000

    test: fix n3 type errors due to version mismatch

--- src/types/n3.d.ts
+++ src/types/n3.d.ts
@@ -1,44 +1,127 @@
-/* file : n3/index.d.ts
-MIT License
+// Type definitions for N3 v0.8.1
+// Project: https://github.com/RubenVerborgh/N3.js
+// Definitions by: Fred Eisele <https://github.com/phreed>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-Copyright (c) 2018-2020 Thomas Minier
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
-interface Triple {
-  subject?: string;
-  predicate?: string;
-  object?: string;
-  graph?: string;
-}
+/// <reference types="node" />
 
 declare module "n3" {
-  export class Parser {
-    parse(input: string): Triple[];
-  }
+  import * as fs from "fs";
+  import * as stream from "stream";
 
-  export namespace Util {
-    export function isIRI(term: string): boolean;
-    export function isLiteral(term: string): boolean;
-    export function getLiteralValue(term: string): string;
-    export function getLiteralLanguage(term: string): string;
-    export function getLiteralType(term: string): string;
+  namespace N3 {
+    type ErrorCallback = (err: Error, result: any) => void;
+
+    interface Prefixes {
+      [key: string]: string;
+    }
+
+    interface LiteralValue {
+      value: string | number;
+    }
+
+    interface Triple {
+      subject: string;
+      predicate: string;
+      object: string;
+    }
+
+    interface BlankTriple {
+      predicate: string;
+      object: string;
+    }
+
+    // class Parser {
+    //    constructor(options?: ParserOptions);
+    // }
+    function Parser(options?: ParserOptions): N3Parser;
+    // class StreamParser extends Parser {
+    //    constructor(options?: ParserOptions);
+    //}
+    function StreamParser(options?: ParserOptions): N3StreamParser;
+
+    interface ParserOptions {
+      format?: string;
+      prefixes?: string[];
+    }
+
+    interface ParseCallback {
+      (error: Error, triple: Triple, prefixes: Prefixes): void;
+    }
+
+    interface Logger {
+      (message?: any, ...optionalParams: any[]): void;
+    }
+    interface N3Parser {
+      parse(input: string): Triple[];
+      parse(input: string, callback: ParseCallback): void;
+      parse(subject: string, predicate: string, object: string): void;
+      parse(triple: Triple): void;
+      parse(stream: fs.ReadStream, log: Logger): void;
+    }
+
+    interface N3StreamParser extends N3Parser {
+      pipe(consumer: stream.Writable | N3StreamWriter): void;
+    }
+
+    // interface fs.ReadStream {
+    //     pipe(parser: StreamParser): void;
+    // }
+
+    function Writer(options: WriterOptions): N3Writer;
+    function Writer(fd: any, options: WriterOptions): N3Writer;
+
+    function StreamWriter(options: WriterOptions): N3StreamWriter;
+
+    interface N3Writer {
+      addTriple(subject: string, predicate: string, object: string): void;
+      addTriple(subject: string, predicate: string, object: string[]): void;
+      addTriple(triple: Triple): void;
+      removeTriple(subject: string, predicate: string, object: string): void;
+      removeTriples(triples: Triple[]): void;
+      getTriples(
+        subject?: string | null,
+        predicate?: string | null,
+        object?: string | null
+      ): Triple[];
+      countTriples(
+        subject?: string | null,
+        predicate?: string | null,
+        object?: string | null
+      ): number;
+      end(err?: ErrorCallback, result?: any): void;
+      blank(ns: string, name: string): string;
+      blank(triple: BlankTriple[]): string;
+      list(triple: string[]): string[];
+    }
+
+    interface N3StreamWriter extends N3Writer {
+      pipe(consumer: stream.Writable): void;
+    }
+
+    interface WriterOptions {
+      format?: string;
+      prefixes?: Prefixes;
+    }
+
+    interface N3StoreWriter extends N3Writer {
+      find(subject: string, predicate: string, object: string): Triple[];
+    }
+    function Store(): N3StoreWriter;
+
+    namespace Util {
+      function createLiteral(value: any): string;
+      function createLiteral(value: any, type: string): string;
+
+      function isIRI(value: string): boolean;
+      function isLiteral(value: string): boolean;
+      function getLiteralValue(value: string): string;
+      function getLiteralLanguage(value: string): string;
+      function getLiteralType(value: string): string;
+      function isBlank(value: string): boolean;
+      function isPrefixedName(name: string): boolean;
+      function expandPrefixedName(name: string, prefixes: Prefixes): string;
+    }
   }
+  export = N3;
 }
