013071bed3cd4a34cf1ec6920a62b3b065927fa2
jahed.dev <3330963+jahed@users.noreply.github.com>
2025-07-28 21:11:13 +0000

    build: export all modules

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,5 @@
 // SPDX-License-Identifier: MIT
-// stages builders
-import { SPARQL_OPERATION } from "./engine/plan-builder.ts";
-import AggregateStageBuilder from "./engine/stages/aggregate-stage-builder.ts";
-import BGPStageBuilder from "./engine/stages/bgp-stage-builder.ts";
-import BindStageBuilder from "./engine/stages/bind-stage-builder.ts";
-import DistinctStageBuilder from "./engine/stages/distinct-stage-builder.ts";
-import FilterStageBuilder from "./engine/stages/filter-stage-builder.ts";
-import GlushkovStageBuilder from "./engine/stages/glushkov-executor/glushkov-stage-builder.ts";
-import GraphStageBuilder from "./engine/stages/graph-stage-builder.ts";
-import MinusStageBuilder from "./engine/stages/minus-stage-builder.ts";
-import OptionalStageBuilder from "./engine/stages/optional-stage-builder.ts";
-import OrderByStageBuilder from "./engine/stages/orderby-stage-builder.ts";
-import ServiceStageBuilder from "./engine/stages/service-stage-builder.ts";
-import UnionStageBuilder from "./engine/stages/union-stage-builder.ts";
-import UpdateStageBuilder from "./engine/stages/update-stage-builder.ts";
-
-const stages = {
-  SPARQL_OPERATION,
-  AggregateStageBuilder,
-  BGPStageBuilder,
-  BindStageBuilder,
-  DistinctStageBuilder,
-  FilterStageBuilder,
-  GlushkovStageBuilder,
-  GraphStageBuilder,
-  MinusStageBuilder,
-  ServiceStageBuilder,
-  OptionalStageBuilder,
-  OrderByStageBuilder,
-  UnionStageBuilder,
-  UpdateStageBuilder,
-};
-
-// base types
 export { default as ExecutionContext } from "./engine/context/execution-context.ts";
-export { PlanBuilder } from "./engine/plan-builder.ts";
-export { BindingBase, Bindings } from "./rdf/bindings.ts";
-export { default as Dataset } from "./rdf/dataset.ts";
-export { default as Graph } from "./rdf/graph.ts";
-export { default as HashMapDataset } from "./rdf/hashmap-dataset.ts";
-// pipeline
 export {
   PipelineEngine,
   type PipelineInput,
@@ -49,13 +9,27 @@ export {
 export { Pipeline } from "./engine/pipeline/pipeline.ts";
 export { default as RxjsPipeline } from "./engine/pipeline/rxjs-pipeline.ts";
 export { default as VectorPipeline } from "./engine/pipeline/vector-pipeline.ts";
-// RDF terms Utilities
-export * as rdf from "./utils/rdf.ts";
-// Formatters
+export { PlanBuilder, SPARQL_OPERATION } from "./engine/plan-builder.ts";
+export { default as AggregateStageBuilder } from "./engine/stages/aggregate-stage-builder.ts";
+export { default as BGPStageBuilder } from "./engine/stages/bgp-stage-builder.ts";
+export { default as BindStageBuilder } from "./engine/stages/bind-stage-builder.ts";
+export { default as DistinctStageBuilder } from "./engine/stages/distinct-stage-builder.ts";
+export { default as FilterStageBuilder } from "./engine/stages/filter-stage-builder.ts";
+export { default as GlushkovStageBuilder } from "./engine/stages/glushkov-executor/glushkov-stage-builder.ts";
+export { default as GraphStageBuilder } from "./engine/stages/graph-stage-builder.ts";
+export { default as MinusStageBuilder } from "./engine/stages/minus-stage-builder.ts";
+export { default as OptionalStageBuilder } from "./engine/stages/optional-stage-builder.ts";
+export { default as OrderByStageBuilder } from "./engine/stages/orderby-stage-builder.ts";
+export { default as ServiceStageBuilder } from "./engine/stages/service-stage-builder.ts";
+export { default as UnionStageBuilder } from "./engine/stages/union-stage-builder.ts";
+export { default as UpdateStageBuilder } from "./engine/stages/update-stage-builder.ts";
 export {
   csvFormatter as CSVFormat,
   tsvFormatter as TSVFormat,
 } from "./formatters/csv-tsv-formatter.ts";
 export { default as JsonFormat } from "./formatters/json-formatter.ts";
-
-export { stages };
+export { BindingBase, Bindings } from "./rdf/bindings.ts";
+export { default as Dataset } from "./rdf/dataset.ts";
+export { default as Graph } from "./rdf/graph.ts";
+export { default as HashMapDataset } from "./rdf/hashmap-dataset.ts";
+export * from "./utils/rdf.ts";
